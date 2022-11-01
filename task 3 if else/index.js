ca39d6ff2d651f4f3a2718d6512795735ac642dd
hammadshaikho <117080932+hammadshaikho@users.noreply.github.com>
2022-11-01 17:51:35 +0500

    Add files via upload

---
+++ task 3 if else/index.js
@@ -0,0 +1,17 @@
+function con(){
+    let inp1 = document.getElementById("inp1")
+    let inpint = parseInt(inp1.value)
+    
+    if(inpint>=18)
+    {
+        console.log("okay")
+
+
+    }
+    else {
+        console.log ("not okay")
+
+    }
+    
+    
+}
